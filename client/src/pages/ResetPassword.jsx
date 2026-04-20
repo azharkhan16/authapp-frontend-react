b52c9bd1887a0d534387f04d3889830cb39a55dc
azharkhan16 <azharkhan75447355@gmail.com>
2026-04-20 11:50:41 +0530

    feat: implement complete reset password flow with email OTP verification and new password submission.

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -3,10 +3,11 @@ import { assets } from "../assets/assets.js";
 import { useContext, useRef, useState } from "react";
 import { AppContext } from "../context/AppContext.jsx";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const ResetPassword = () => {
 
-  const inputsRef = useRef([]);
+  const inputRef = useRef([]);
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState("");
@@ -18,8 +19,77 @@ const ResetPassword = () => {
 
   axios.defaults.withCredentials = true;
 
-  
+  const handleChange = (e, index) => {
+    const value = e.target.value.replace(/\D/, "");
+    e.target.value = value;
+    if (value && index < 5) {
+        inputRef.current[index + 1].focus();
+    }
+  }
 
+  const handleKeyDown = (e, index) => {
+    if (e.key === "Backspace" && !e.target.value && index > 0) {
+        inputRef.current[index - 1].focus();
+    }
+  }
+
+  const handlePaste = (e) => {
+      e.preventDefault();
+      const paste = e.clipboardData.getData("text").slice(0, 6).split("");
+      paste.forEach((digit, i) => {
+          if (inputRef.current[i]) {
+              inputRef.current[i].value = digit;
+          }
+      });
+      const next = paste.length < 6 ? paste.length : 5;
+      inputRef.current[next].focus();    
+  }
+
+  const onSubmitEmail = async (e) => {
+      e.preventDefault();
+      setLoading(true);
+      try {
+          const response = await axios.post(backendURL + "/send-reset-otp?email="+email);
+          if (response.status ===200) {
+              toast.success("Password reset OTP sent successfully.");
+              setIsEmailSent(true);
+          } else {
+              toast.error("Something went wrong, please try again.");
+          }
+      } catch(error) {
+           toast.error(error.message);
+      } finally {
+           setLoading(false);
+      }
+  }
+
+  const handleVerify = () => {
+      const otp = inputRef.current.map((input) => input.value).join("");
+      if (otp.length !==6) {
+          toast.error("Please enter all 6 digits of the OTP.");
+          return;
+      }
+      setOtp(otp);
+      setIsOtpSubmitted(true);
+  }
+
+  const onSubmitNewPassword = async (e) => {
+      e.preventDefault();
+      setLoading(true);
+      try {
+          const response = await axios.post(backendURL + "/reset-password", {email, otp, newPassword});
+          if (response.status ===200) {
+              toast.success("Password reset sucessfully.");
+              navigate("/login");
+          } else {
+                toast.error("Something went wrong, please try again.");        
+          }
+      } catch (error) {
+          toast.error(error.message);        
+      } finally {
+           setLoading(false);
+      }
+  }
 
   return (
       <div className="d-flex align-items-center justify-content-center vh-100 position-relative"
@@ -35,7 +105,8 @@ const ResetPassword = () => {
               <div className="rounded-4 p-5 text-center bg-white" style={{width:'100%', maxWidth:'400px'}}>
                   <h4 className="mb-2">Reset Password</h4>
                   <p className="mb-4">Enter your registered email address</p>
-                  <form>
+
+                  <form onSubmit={onSubmitEmail}>
                       <div className="input-group mb-4 bg-secondary bg-opacity-10 rounded-pill">
                           <span className="input-group-text bg-transparent border-0 ps-4">
                               <i className="bi bi-envelope"></i>
@@ -49,13 +120,67 @@ const ResetPassword = () => {
                                  required
                           />
                       </div>
-                      <button className="btn btn-primary w-100 py-2" type="submit">
-                          Submit
+                      <button className="btn btn-primary w-100 py-2" type="submit" disabled={loading}>
+                          {loading ? "Loading..." : "Submit"}
                       </button>
                   </form>
               </div>           
           )}
+          {/* Reset OTP card */}  
+          {!isOtpSubmitted && isEmailSent && (
+              <div className="p-5 rounded-4 shadow bg-white" style={{width: "400px"}}>
+                   <h4 className=" text-center fw-bold mb-2">Email Verify OTP</h4>
+                   <p className="text-center text-white-50 mb=4">
+                       Enter the 6 digit code sent to your email.
+                   </p>
 
+                   <div className="d-flex justify-content-between gap-2 mb-4 text-center text-white mb-2">
+                       {[...Array(6)].map((_, i) => (
+                           <input
+                               key={i}
+                               type="text"
+                               maxLength={1}
+                               className="form-control text-center fs-4 otp-input"
+                               ref = {(el) => (inputRef.current[i] = el)}
+                               onChange={(e) => handleChange(e, i)}
+                               onKeyDown={(e) => handleKeyDown(e, i)}
+                               onPaste={handlePaste}
+                            />   
+                       ))}
+                   </div>
+
+                   <button className="btn btn-primary w-100 fw-semibold" disabled={loading} onClick={handleVerify}>
+                       {loading ? "Verifying..." : "Verify email"}
+                   </button>  
+              </div>                 
+          )} 
+          {/* New password card */}
+          {isOtpSubmitted && isEmailSent && (
+              <div className="rounded-4 p-4 text-center bg-white" style={{width:'100%', maxWidth:'400px'}}>
+                  <h4>New Password</h4>
+                  <p className="mb-4">Enter the new password below</p>
+
+                  <form onSubmit={onSubmitNewPassword}>
+                      <div className="input-group mb-4 bg-secondary bg-opacity-10 rounded-pill">
+                          <span className="input-group-text bg-transparent border-0 ps-4">
+                             <i className="bi bi-person-fill-lock"></i>
+                          </span>
+                          <input 
+                             type="password"
+                             className="form-control bg-transparent border-0 ps-1 pe-4 rounded-end"
+                             placeholder="************"
+                             style={{height: "50px"}}
+                             onChange={(e) => setNewPassword(e.target.value)}
+                             value={newPassword}
+                             required 
+                          />
+                      </div>
+                      <button type="submit" className="btn btn-primary w-100" disabled={loading}>
+                          {loading ? "Loading..." : "Submit"}
+                      </button>
+                  </form>
+            </div>
+         )}          
       </div>
   )
 }
